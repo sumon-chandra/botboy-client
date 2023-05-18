10bb8b4072648f6c2204953ec23cd15e54ade48e
sumon-chandra <topu199041@gmail.com>
2023-05-18 23:17:18 +0600

    Create 404 page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,7 +1,19 @@
+import { Link } from "react-router-dom";
+
 const Error = () => {
   return (
-    <div>
-      <h3>404 Error</h3>
+    <div className="min-w-full min-h-screen grid place-items-center">
+      <div className="flex items-center gap-x-6">
+        <div className="text-4xl font-bold text-gray-700 border-r-8 border-gray-700 p-3 text-right">
+          <p className="lg:text-7xl text-5xl">404</p>
+          <p>Error</p>
+        </div>
+        <div className="text-2xl ">
+
+        <p className="text-gray-500">Page not found</p>
+        <p className="text-gray-500">Go to <Link to="/" className="underline font-semibold italic">homepage</Link> </p>
+        </div>
+      </div>
     </div>
   );
 };
