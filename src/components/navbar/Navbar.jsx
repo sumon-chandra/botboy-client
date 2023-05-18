3a84ecb69c014fed53ee9604a832bee7771a416e
sumon-chandra <topu199041@gmail.com>
2023-05-18 23:04:28 +0600

    Handle logout

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,15 @@
 import NavItem from "./NavItem";
 import logo from "../../assets/logo.png";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useContext } from "react";
 import { AuthContext } from "../../context-providers/AuthProvider";
 const Navbar = () => {
-  const { user } = useContext(AuthContext);
+  const { user, logout } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    logout().then(() => navigate("/login"))
+  };
   return (
     <div className="navbar bg-slate-100 lg:px-20 px-4">
       <div className="navbar-start">
@@ -79,7 +84,7 @@ const Navbar = () => {
               <li>
                 <NavItem value="My Toys" to="/my-toys" />
               </li>
-              <button className="btn btn-sm px-4 py-2 normal-case text-white bg-gradient-to-br from-mainColor to-secColor hover:bg-gradient-to-tl border-0">
+              <button onClick={handleLogout} className="btn btn-sm px-4 py-2 normal-case text-white bg-gradient-to-br from-mainColor to-secColor hover:bg-gradient-to-tl border-0">
                 Logout
               </button>
             </ul>
