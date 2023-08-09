4c0a17b862141ffeab9904a97ed6e6878c5411ce
Sumon Chandra <topu199041@gmail.com>
2023-08-09 21:17:55 +0600

    Work on the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,58 +1,71 @@
 import { Link } from "react-router-dom";
 import logo from "../assets/logo.png";
-import { FaYoutube, FaTwitter, FaFacebook} from "react-icons/fa"
+import {
+  FaYoutube,
+  FaTwitter,
+  FaFacebook,
+  FaMap,
+  FaPhone,
+  FaEnvelope,
+  FaPinterest,
+  FaInstagram,
+} from "react-icons/fa";
 const Footer = () => {
   return (
-    <footer className="bg-gradient-to-br from-mainColor to-secColor text-white">
-      <section className="footer p-10">
-        <div >
-          <span className="footer-title">Services</span>
-          <a className="link link-hover">Branding</a>
-          <a className="link link-hover">Design</a>
-          <a className="link link-hover">Marketing</a>
-          <a className="link link-hover">Advertisement</a>
-        </div>
+    <footer className="text-white bg-gray-800 bg-gradient-to-br">
+      <section className="grid-cols-1 gap-10 px-4 py-8 lg:grid-cols-4 footer lg:p-20">
         <div>
-          <span className="footer-title">Company</span>
-          <a className="link link-hover">About us</a>
-          <a className="link link-hover">Contact</a>
-          <a className="link link-hover">Jobs</a>
-          <a className="link link-hover">Press kit</a>
-        </div>
-        <div>
-          <span className="footer-title">Legal</span>
-          <a className="link link-hover">Terms of use</a>
-          <a className="link link-hover">Privacy policy</a>
-          <a className="link link-hover">Cookie policy</a>
-        </div>
-      </section>
-      <section className="footer px-10 py-4 border-t border-indigo-600">
-        <div className="space-y-2">
-          <a href="#" className="flex items-center select-none text-xl">
-            <img className="w-10 mr-1" src={logo} alt="logo" />
-            <span className="font-bold text-gray-800">Botboy</span>
-          </a>
-          <p>
-            ACME Industries Ltd. <br />
-            Providing reliable superhero toys since 1992
+          <Link to="/" className="flex items-center select-none ps-4 lg:ps-0">
+            <img className="w-8 mr-1" src={logo} alt="logo" />
+            <span className="text-xl font-bold text-white uppercase lg:text-3xl font-head">
+              Botboy
+            </span>
+          </Link>
+          <p className="text-xs lg:text-lg">
+            Nunc consequat interdum varius sit amet mattis. Ac tortor vitae
+            purus faucibus ornare.
           </p>
         </div>
-        <div className="md:place-self-center md:justify-self-end">
-          <div className="grid grid-flow-col gap-x-6">
-            <a>
-              <FaYoutube className="text-2xl shadow-lg cursor-pointer" />
-            </a>
-            <a>
-              <FaFacebook className="text-2xl shadow-lg cursor-pointer" />
-            </a>
-            <a>
-             <FaTwitter className="text-2xl shadow-lg cursor-pointer" />
-            </a>
+        <div>
+          <h3 className="pb-4 text-2xl font-bold text-gray-500">We are here</h3>
+          <p className="text-lg">
+            No. 58 A, East Madison Street, Blatimore, MD, USA 4508
+          </p>
+          <button className="footer-btn">
+            <FaMap />
+            <span className="text-sm">View on map</span>
+          </button>
+        </div>
+        <div>
+          <h3 className="pb-4 text-2xl font-bold text-gray-500">Contact Us</h3>
+          <button className="footer-btn">
+            <FaPhone />
+            <span>0000 - 123 - 347789</span>
+          </button>
+          <button className="footer-btn">
+            <FaEnvelope />
+            <span>info@example.com</span>
+          </button>
+        </div>
+        <div>
+          <h3 className="pb-4 text-2xl font-bold text-gray-500">
+            Get us on social
+          </h3>
+          <div className="flex items-center gap-4">
+            <button className="px-2 footer-btn text-mainColor">
+              <FaFacebook />
+            </button>
+            <button className="px-2 footer-btn text-mainColor">
+              <FaPinterest />
+            </button>
+            <button className="px-2 footer-btn text-mainColor">
+              <FaInstagram />
+            </button>
           </div>
         </div>
       </section>
-      <div className="text-center pb-3">
-        <p>Copyright © 2023 - All right reserved by ACME Industries Ltd</p>
+      <div className="pb-3 text-xs text-center">
+        <p>Copyright © 2023 - All right reserved by BOTBOY Industries Ltd</p>
       </div>
     </footer>
   );
